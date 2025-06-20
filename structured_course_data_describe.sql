9c8e3e65c846a51e6b8dedc3f355dd2af90ee26e
Michael Vera <michaelvera717@gmail.com>
2025-06-19 21:13:07 -0400

    Cycling course exploratory data analysis.

---
+++ structured_course_data_describe.sql
@@ -0,0 +1,8 @@
+select count(*) as cnt from cycling.structured_course_data; 
+--8094
+
+--View several lines
+select * from cycling.structured_course_data limit 10;
+
+--Get table's columns names
+PRAGMA table_info(structured_course_data);
